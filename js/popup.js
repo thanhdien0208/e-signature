9a5d66a8f2fa5159aecf58bee43b1112679b5889
thanhdien0208 <diennguyen.blog@gmail.com>
2021-06-20 21:37:42 +0700

    upload

---
+++ js/popup.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+        
+    $(".popup").hide();
+
+    $("#button1").click(function(e) {
+
+        openPopup();
+
+    });
+
+    $(".close").click(function (e) {
+
+        closePopup();
+
+        e.preventDefault();
+
+    });
+
+    $("#background").click(function () {
+
+        closePopup();
+
+    });
+
+});
+
+function openPopup(){
+
+    var dheight = document.body.clientHeight;
+
+    var dwidth = document.body.clientWidth;
+
+    $("#background").width(dwidth).height(dheight);
+
+    $("#background").fadeTo("slow",0.8);
+
+    var $popup1=$("#popup1");
+
+    $popup1.css("top", (dheight-$popup1.height())/2);
+
+    $popup1.css("left",(dwidth-$popup1.width())/2);
+
+    $popup1.fadeIn();
+
+}
+
+function closePopup(){
+
+    $("#background").fadeOut();
+
+    $(".popup").hide();
+
+}
