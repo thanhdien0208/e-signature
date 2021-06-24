a043ca2af806446116ebbe2cef0793678496af94
thanhdien0208 <diennguyen.blog@gmail.com>
2021-06-25 01:28:13 +0700

    upload

--- js/canvas.js
+++ js/canvas.js
@@ -91,26 +91,26 @@
     }
   
 // Prevent scrolling when touching the canvas
-document.body.addEventListener("touchstart", function(e) {
-    if (e.target == canvas) {
-      e.preventDefault();
-    }
-  }, {passive: false});
-  document.body.addEventListener("touchend", function(e) {
-    if (e.target == canvas) {
-      e.preventDefault();
-    }
-  }, {passive: false});
-  document.body.addEventListener("touchmove", function(e) {
-    if (e.target == canvas) {
-      e.preventDefault();
-    }
-  }, {passive: false});
+// document.body.addEventListener("touchstart", function(e) {
+//     if (e.target == canvas) {
+//       e.preventDefault();
+//     }
+//   }, {passive: false});
+//   document.body.addEventListener("touchend", function(e) {
+//     if (e.target == canvas) {
+//       e.preventDefault();
+//     }
+//   }, {passive: false});
+//   document.body.addEventListener("touchmove", function(e) {
+//     if (e.target == canvas) {
+//       e.preventDefault();
+//     }
+//   }, {passive: false});
 
-  (function drawLoop() {
-    requestAnimFrame(drawLoop);
-    renderCanvas();
-  })();
+//   (function drawLoop() {
+//     requestAnimFrame(drawLoop);
+//     renderCanvas();
+//   })();
 
 //   function clearCanvas() {
 //     canvas.width = canvas.width;
